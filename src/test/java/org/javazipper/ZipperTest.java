615cd288bc1932e09f24c90ede6f7327e5f17ec1
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-06 19:58:39 +0100

    Add basic JUnit unit test

---
+++ src/test/java/org/javazipper/ZipperTest.java
@@ -0,0 +1,22 @@
+package org.javazipper;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZipperTest {
+    @Test
+    void shouldCorrectlyZipMultipleLists() {
+        //given
+        final var input = List.of(List.of(1, 2, 3), List.of(4, 5, 6), List.of(7, 8, 9));
+
+        //when
+        final var output = Zipper.zip(input);
+
+        //then
+        final var expectedOutput = List.of(List.of(1, 4, 7), List.of(2, 5, 8), List.of(3, 6, 9));
+        assertEquals(expectedOutput, output);
+    }
+}
