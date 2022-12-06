c8a5d9b0377cb5a6abe101ee9a7a258e20702e6c
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-06 19:53:04 +0100

    Add basic "zip" implementation

---
+++ src/main/java/org/javazipper/Zipper.java
@@ -0,0 +1,16 @@
+package org.javazipper;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Zipper {
+    public static <T> List<List<T>> zip(final List<List<T>> lists) {
+        if (lists.isEmpty()) {
+            return Collections.emptyList();
+        }
+        final var size = lists.stream().min(Comparator.comparingInt(List::size)).get().size();
+        return IntStream.range(0, size).mapToObj(i -> lists.stream().map(list -> list.get(i)).toList()).toList();
+    }
+}
