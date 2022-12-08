d043b83c948bab9dc66a8a17d46559479ac77a0c
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-08 20:58:40 +0100

    Add javadocs

--- src/main/java/org/zipper/Zipper.java
+++ src/main/java/org/zipper/Zipper.java
@@ -5,20 +5,64 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collector;
 
+/**
+ * Utility class providing various static methods performing Python-style "zip" operation on multiple lists of elements.
+ * For example, converting lists [[1, 2, 3], [4, 5, 6]] into [[1, 4], [2, 5], [3, 6]].
+ * Effectively it will select columns from provided 2D arrangement.
+ * <p>
+ * If not all lists are of the same size, then smallest possible subset is selected.
+ * For example, in [[1, 2, 3, 4], [5, 6, 7], [8, 9]] smallest list has size 2, which results in [[1, 5, 8], [2, 6, 9]].
+ * It will be equivalent to zipping [[1, 2], [5, 6], [8, 9]].
+ * <p>
+ * If any of inner list has size 0, or empty list is provided, then result will be an empty list.
+ */
 public final class Zipper {
     private Zipper() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Zip provided list of lists. Equivalent to:
+     * <pre>
+     *     lists.stream().collect(Zipper.zipCollector());
+     * </pre>
+     * See {@link Zipper} for more details regarding zipping.
+     *
+     * @param lists list of lists to zip
+     * @param <T>   type stored in provided lists
+     * @return zipped input
+     */
     public static <T> List<List<T>> zip(final List<List<T>> lists) {
         return lists.stream().collect(zipCollector());
     }
 
+    /**
+     * Zip provided lists. Lists can be provided as an array, or as multiple arguments. Equivalent to:
+     * <pre>
+     *     Arrays.stream(lists).collect(Zipper.zipCollector());
+     *     Zipper.zip(Arrays.asList(lists));
+     * </pre>
+     * See {@link Zipper} for more details regarding zipping.
+     *
+     * @param lists multiple lists to zip, or an array of lists
+     * @param <T>   type stored in provided lists
+     * @return zipped input
+     */
     @SafeVarargs
     public static <T> List<List<T>> zip(final List<T>... lists) {
         return Arrays.stream(lists).collect(zipCollector());
     }
 
+    /**
+     * Stream API collector which can be used for zipping stream of lists. For example:
+     * <pre>
+     *     Stream.of(List.of(1, 2, 3), List.of(4, 5, 6)).collect(zipCollector());
+     * </pre>
+     * will result in [[1, 4], [2, 5], [3, 6]].
+     *
+     * @param <T> type stored in lists in stream
+     * @return collector used to zipping lists as Stream API elements
+     */
     public static <T> Collector<List<T>, List<List<T>>, List<List<T>>> zipCollector() {
         return Collector.of(ArrayList::new, Zipper::zipCollectorAccumulator, Zipper::zipCollectorCombiner);
     }
